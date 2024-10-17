9a8ce29db892003acf3d09eb808702eab024aec2
Michael Adkins <michaelpauladkins@gmail.com>
2024-10-17 11:25:28 -0400

    finally fixed the issues that kept the tags from showing.

--- SecondCapstone/SecondCapstone/client/src/App.jsx
+++ SecondCapstone/SecondCapstone/client/src/App.jsx
@@ -1,20 +1,15 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
-import EntryList from './Components/Entry/EntryList.jsx';
-import ApplicationViews from './Components/ApplicationViews';
+import ApplicationViews from './Components/ApplicationViews'; // Handle all routes here
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Navbar />
-                {/* Wrap Route inside Routes and use element instead of component */}
-                <Routes>
-                    <Route path="/entries/latest" element={<EntryList />} />
-                </Routes>
                 <ApplicationViews />
             </div>
         </Router>
