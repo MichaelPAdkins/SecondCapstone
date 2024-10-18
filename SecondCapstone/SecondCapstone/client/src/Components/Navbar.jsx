490d9cd763681d9c5cc37e341718527eb219737b
Michael Adkins <michaelpauladkins@gmail.com>
2024-10-18 15:55:30 -0400

    Viable demo branch.

--- SecondCapstone/SecondCapstone/client/src/Components/Navbar.jsx
+++ SecondCapstone/SecondCapstone/client/src/Components/Navbar.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className="navbar">
-            <h2>SecondCapstone</h2>
+            <h2>Archives Mapped! </h2>
             <nav>
                 <Link to="/entries/latest" style={{ color: 'white', textDecoration: 'none', marginRight: '15px' }}>Latest Entries</Link>
-                <Link to="/entry/create" style={{ color: 'white', textDecoration: 'none', marginRight: '15px' }}>Add Entry</Link>
+                {/* <Link to="/entry/create" style={{ color: 'white', textDecoration: 'none', marginRight: '15px' }}>Add Entry</Link> */}
                 <Link to="/cameras" style={{ color: 'white', textDecoration: 'none', marginRight: '15px' }}>Cameras</Link>
                 <Link to="/tags/list" style={{ color: 'white', textDecoration: 'none', marginRight: '15px' }}>Tag List</Link>
                 <Link to="/locations/list" style={{ color: 'white', textDecoration: 'none', marginRight: '15px' }}>Locations</Link>
